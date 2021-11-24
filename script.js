57945023d20aa66baf4bcba3fd772ee0f938fa15
Hernando Abella <hernandoabella@gmail.com>
2021-11-24 02:00:37 -0500

    testimonios agregados

---
+++ script.js
@@ -0,0 +1,12 @@
+// Testimonials carousel
+$(".testimonials-carousel").owlCarousel({
+    center: true,
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+        0:{
+            items:1
+        }
+    }
+});
