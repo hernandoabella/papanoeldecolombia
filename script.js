13ef78b4f64c8992a0f0ca81757d702c1acc2188
Hernando Abella <hernandoabella@gmail.com>
2021-11-26 18:37:34 -0500

    fixing some bugs and adding new items

--- script.js
+++ script.js
@@ -10,3 +10,38 @@ $(".testimonials-carousel").owlCarousel({
         }
     }
 });
+   //TESTIMONIAL SLIDER
+   if ($(".ts-testimonial-slide").length > 0) {
+    
+    $(".ts-testimonial-slide").owlCarousel({
+        autoPlay: 4000,
+        slideSpeed: 1000,
+        navigation: false,
+        pagination: true,
+        singleItem: true
+    });
+};
+
+$(document).ready(function () {
+    var silder = $(".owl-carousel");
+    silder.owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: false,
+    items: 1,
+    stagePadding: 20,
+    center: true,
+    nav: false,
+    margin: 50,
+    dots: true,
+    loop: true,
+    responsive: {
+    0: { items: 1 },
+    480: { items: 2 },
+    575: { items: 2 },
+    768: { items: 2 },
+    991: { items: 3 },
+    1200: { items: 4 }
+    }
+    });
+    });
