fb4ff2d04e642be4c009b11b685f0ff17bc0e2e9
Hernando Abella <hernandoabella@gmail.com>
2021-11-27 02:02:32 -0500

    agregando galeria

--- script.js
+++ script.js
@@ -10,38 +10,96 @@ $(".testimonials-carousel").owlCarousel({
         }
     }
 });
-   //TESTIMONIAL SLIDER
-   if ($(".ts-testimonial-slide").length > 0) {
-    
-    $(".ts-testimonial-slide").owlCarousel({
-        autoPlay: 4000,
-        slideSpeed: 1000,
-        navigation: false,
-        pagination: true,
-        singleItem: true
-    });
-};
 
-$(document).ready(function () {
-    var silder = $(".owl-carousel");
-    silder.owlCarousel({
-    autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: false,
-    items: 1,
-    stagePadding: 20,
-    center: true,
-    nav: false,
-    margin: 50,
-    dots: true,
-    loop: true,
-    responsive: {
-    0: { items: 1 },
-    480: { items: 2 },
-    575: { items: 2 },
-    768: { items: 2 },
-    991: { items: 3 },
-    1200: { items: 4 }
+let modalId = $('#image-gallery');
+
+$(document)
+  .ready(function () {
+
+    loadGallery(true, 'a.thumbnail');
+
+    //This function disables buttons when needed
+    function disableButtons(counter_max, counter_current) {
+      $('#show-previous-image, #show-next-image')
+        .show();
+      if (counter_max === counter_current) {
+        $('#show-next-image')
+          .hide();
+      } else if (counter_current === 1) {
+        $('#show-previous-image')
+          .hide();
+      }
     }
-    });
-    });
+
+    /**
+     *
+     * @param setIDs        Sets IDs when DOM is loaded. If using a PHP counter, set to false.
+     * @param setClickAttr  Sets the attribute for the click handler.
+     */
+
+    function loadGallery(setIDs, setClickAttr) {
+      let current_image,
+        selector,
+        counter = 0;
+
+      $('#show-next-image, #show-previous-image')
+        .click(function () {
+          if ($(this)
+            .attr('id') === 'show-previous-image') {
+            current_image--;
+          } else {
+            current_image++;
+          }
+
+          selector = $('[data-image-id="' + current_image + '"]');
+          updateGallery(selector);
+        });
+
+      function updateGallery(selector) {
+        let $sel = selector;
+        current_image = $sel.data('image-id');
+        $('#image-gallery-title')
+          .text($sel.data('title'));
+        $('#image-gallery-image')
+          .attr('src', $sel.data('image'));
+        disableButtons(counter, $sel.data('image-id'));
+      }
+
+      if (setIDs == true) {
+        $('[data-image-id]')
+          .each(function () {
+            counter++;
+            $(this)
+              .attr('data-image-id', counter);
+          });
+      }
+      $(setClickAttr)
+        .on('click', function () {
+          updateGallery($(this));
+        });
+    }
+  });
+
+// build key actions
+$(document)
+  .keydown(function (e) {
+    switch (e.which) {
+      case 37: // left
+        if ((modalId.data('bs.modal') || {})._isShown && $('#show-previous-image').is(":visible")) {
+          $('#show-previous-image')
+            .click();
+        }
+        break;
+
+      case 39: // right
+        if ((modalId.data('bs.modal') || {})._isShown && $('#show-next-image').is(":visible")) {
+          $('#show-next-image')
+            .click();
+        }
+        break;
+
+      default:
+        return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+  });
